cce9ba47b41b2c047087dccd66ffee33d8789d33
illia hrushchenko <gr.illya13@gmail.com>
2023-07-28 19:18:03 +0300

    Added CreateUserTests.cs

---
+++ tests/Application.IntegrationTests/Users/Commands/CreateUser/CreateUserTests.cs
@@ -0,0 +1,19 @@
+using Application.Users.Commands.CreateUser;
+using Domain.Entities;
+using FluentAssertions;
+
+namespace Application.IntegrationTests.Users.Commands.CreateUser;
+
+public class CreateUserTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldCreateUser()
+    {
+        var userId = await Testing.SendAsync(new CreateUserCommand("mail@gmail.com",
+            "1111"));
+
+        var user = await Testing.FindAsync<User>(userId);
+
+        user.Should().NotBeNull();
+    }
+}
