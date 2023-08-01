62940d9cb4339e020922b1e241323f808093fb13
illia hrushchenko <gr.illya13@gmail.com>
2023-08-01 16:28:15 +0300

    Renamed GetAuthToken method

--- src/Application/Common/Interfaces/IIdentityService.cs
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -1,8 +1,10 @@
+using Application.Users.Queries.GetAuthToken;
+
 namespace Application.Common.Interfaces;
 
 public interface IIdentityService
 {
     Task<int> CreateUserAsync(string email, string password);
-    Task<string> GetToken(string email, string password);
+    Task<string> GetAuthTokenAsync(string email, string password);
     
 }
