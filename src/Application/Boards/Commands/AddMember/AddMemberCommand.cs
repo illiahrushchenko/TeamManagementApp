e1bd05933fa964d8f9d3f0800908a37ea5c1403b
illia hrushchenko <gr.illya13@gmail.com>
2023-08-13 14:56:37 +0300

    Implemented AddMemberCommand.cs

--- src/Application/Boards/Commands/AddMember/AddMemberCommand.cs
+++ src/Application/Boards/Commands/AddMember/AddMemberCommand.cs
@@ -1,13 +1,36 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Boards.Commands.AddMember;
 
-public record AddMemberCommand(int boardId, string email) : IRequest<int>;
+public record AddMemberCommand(int BoardId, string Email) : IRequest<int>;
 
 public class AddMemberCommandHandler : IRequestHandler<AddMemberCommand, int>
 {
+    private readonly IIdentityService _identityService;
+    private readonly IApplicationDbContext _context;
+
+    public AddMemberCommandHandler(IIdentityService identityService, IApplicationDbContext context)
+    {
+        _identityService = identityService;
+        _context = context;
+    }
+    
     public async Task<int> Handle(AddMemberCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var board = await _context.Boards
+                        .FirstOrDefaultAsync(x => x.Id == request.BoardId, cancellationToken: cancellationToken) ??
+                    throw new NotFoundException(nameof(Board), request.BoardId);
+
+        var user = await _identityService.FindUserByEmailAsync(request.Email) ??
+                   throw new NotFoundException(nameof(User), request.Email);
+        
+        board.Members.Add(user);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return board.Id;
     }
 }
