e97a1c2c539c6f6592ec6f259cabaf18e942d874
illia hrushchenko <gr.illya13@gmail.com>
2023-08-15 13:29:31 +0300

    Added UpdateBoardCommandTests.cs

---
+++ tests/Application.IntegrationTests/Boards/Commands/UpdateBoardCommandTests.cs
@@ -0,0 +1,23 @@
+using Application.Boards.Commands.CreateBoard;
+using Application.Boards.Commands.UpdateBoard;
+using Domain.Entities;
+using FluentAssertions;
+
+namespace Application.IntegrationTests.Boards.Commands;
+
+public class UpdateBoardCommandTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldUpdateBoard()
+    {
+        await Testing.RunAsUserAsync("ddd@gmail.com", "1234");
+
+        var boardId = await Testing.SendAsync(new CreateBoardCommand("old title"));
+        await Testing.SendAsync(new UpdateBoardCommand(boardId, "new title"));
+
+        var board = await Testing.FindAsync<Board>(boardId);
+
+        board.Should().NotBeNull();
+        board!.Title.Should().Be("new title");
+    }
+}
