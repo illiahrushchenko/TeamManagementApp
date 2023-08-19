c8ccb5415e1fd3f645c1fd69406137772e57c8eb
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 21:08:07 +0300

    Implemented UpdateTableCommand

--- src/Application/Tables/Commands/UpdateTable/UpdateTableCommand.cs
+++ src/Application/Tables/Commands/UpdateTable/UpdateTableCommand.cs
@@ -1,13 +1,34 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Tables.Commands.UpdateTable;
 
-public record UpdateTableCommand() : IRequest<int>;
+public record UpdateTableCommand(int TableId, string Title, int BoardId) : IRequest<int>;
 
 public class UpdateTableCommandHandler : IRequestHandler<UpdateTableCommand, int>
 {
-    public Task<int> Handle(UpdateTableCommand request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+
+    public UpdateTableCommandHandler(IApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+    public async Task<int> Handle(UpdateTableCommand request, CancellationToken cancellationToken)
+    {
+        var table = await _context.Tables
+                        .FirstOrDefaultAsync(x => x.Id == request.TableId, cancellationToken: cancellationToken) ??
+                    throw new NotFoundException(nameof(Table), request.TableId);
+
+        table.BoardId = request.BoardId;
+        table.Title = request.Title;
+
+        _context.Tables.Update(table);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return table.Id;
     }
 }
