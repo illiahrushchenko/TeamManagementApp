80f16d5ddbd33c366d3fa32ed5c30752e67a99ba
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 23:25:34 +0300

    Fixed request type

--- src/Application/TableCards/Commands/CreateTableCard/CreateTableCardCommand.cs
+++ src/Application/TableCards/Commands/CreateTableCard/CreateTableCardCommand.cs
@@ -5,9 +5,9 @@ namespace Application.TableCards.Commands.CreateTableCard;
 
 public record CreateTableCardCommand(string Title, string Description, int TableId) : IRequest<int>;
 
-public class CreateTableCardCommandHandler : IRequestHandler<CreateTableCommand, int>
+public class CreateTableCardCommandHandler : IRequestHandler<CreateTableCardCommand, int>
 {
-    public Task<int> Handle(CreateTableCommand request, CancellationToken cancellationToken)
+    public Task<int> Handle(CreateTableCardCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
