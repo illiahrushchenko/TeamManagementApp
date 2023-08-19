9306a6979010fb533cf593f29a903ad68b2251b6
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 12:42:52 +0300

    Implemented CreateTableCommand.cs

--- src/Application/Tables/Commands/CreateTable/CreateTableCommand.cs
+++ src/Application/Tables/Commands/CreateTable/CreateTableCommand.cs
@@ -1,13 +1,47 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Tables.Commands.CreateTable;
 
-public record CreateTableCommand : IRequest<int>;
+public record CreateTableCommand(string Title, int BoardId) : IRequest<int>;
 
 public class CreateTableCommandHandler : IRequestHandler<CreateTableCommand, int>
 {
-    public Task<int> Handle(CreateTableCommand request, CancellationToken cancellationToken)
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IApplicationDbContext _context;
+    private readonly IBoardMembersService _boardMembersService;
+
+    public CreateTableCommandHandler(ICurrentUserService currentUserService, IApplicationDbContext context, IBoardMembersService boardMembersService)
     {
-        throw new NotImplementedException();
+        _currentUserService = currentUserService;
+        _context = context;
+        _boardMembersService = boardMembersService;
+    }
+
+    public async Task<int> Handle(CreateTableCommand request, CancellationToken cancellationToken)
+    {
+        var board = await _context.Boards
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Id == request.BoardId, cancellationToken: cancellationToken) ??
+                    throw new NotFoundException(nameof(Board), request.BoardId);
+
+        if (!await _boardMembersService.UserIsOwner(_currentUserService.UserId, board.Id))
+        {
+            throw new ForbiddenAccessException(nameof(Board), board.Id);
+        }
+        
+        var table = new Table
+        {
+            Title = request.Title,
+            BoardId = request.BoardId
+        };
+
+        await _context.Tables.AddAsync(table, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return table.Id;
     }
 }
