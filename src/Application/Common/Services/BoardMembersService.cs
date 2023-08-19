dcdea33ae3f69c2557ea50fdd50e44ad3584f5b4
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 12:41:47 +0300

    Fixed problem with non-included properties

--- src/Application/Common/Services/BoardMembersService.cs
+++ src/Application/Common/Services/BoardMembersService.cs
@@ -1,19 +1,25 @@
+using Application.Common.Exceptions;
 using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Common.Services;
 
 public class BoardMembersService : IBoardMembersService
 {
-    private readonly IIdentityService _identityService;
+    private readonly IApplicationDbContext _context;
 
-    public BoardMembersService(IIdentityService identityService)
+    public BoardMembersService(IApplicationDbContext context)
     {
-        _identityService = identityService;
+        _context = context;
     }
 
     public async Task<bool> UserIsMember(int userId, int boardId)
     {
-        var user = await _identityService.FindUserByIdAsync(userId);
+        var user = await _context.Users
+                       .Include(x => x.OtherBoards)
+                       .FirstOrDefaultAsync(x => x.Id == userId) ??
+                   throw new NotFoundException(nameof(User), userId);
 
         return user.OtherBoards
             .Any(x => x.Id == boardId);
@@ -21,7 +27,10 @@ public class BoardMembersService : IBoardMembersService
 
     public async Task<bool> UserIsOwner(int userId, int boardId)
     {
-        var user = await _identityService.FindUserByIdAsync(userId);
+        var user = await _context.Users
+                       .Include(x => x.OwnBoards)
+                       .FirstOrDefaultAsync(x => x.Id == userId) ??
+                   throw new NotFoundException(nameof(User), userId);
 
         return user.OwnBoards
             .Any(x => x.Id == boardId);
