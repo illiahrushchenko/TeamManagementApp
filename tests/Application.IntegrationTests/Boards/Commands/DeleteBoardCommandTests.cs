7733afd0f26c8ee36e425aa6470578e68de8a713
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 20:00:18 +0300

    Added DeleteBoardCommandTests

---
+++ tests/Application.IntegrationTests/Boards/Commands/DeleteBoardCommandTests.cs
@@ -0,0 +1,35 @@
+using Application.Boards.Commands.CreateBoard;
+using Application.Boards.Commands.DeleteBoard;
+using Application.Common.Exceptions;
+using Domain.Entities;
+using FluentAssertions;
+
+namespace Application.IntegrationTests.Boards.Commands;
+
+public class DeleteBoardCommandTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidId()
+    {
+        await Testing.RunAsUserAsync("qqq@gmail.com", "1234");
+
+        var command = new DeleteBoardCommand(452);
+
+        await FluentActions.Invoking(() => Testing.SendAsync(command))
+            .Should().ThrowAsync<NotFoundException>();
+    }
+    
+    [Test]
+    public async Task ShouldDeleteBoard()
+    {
+        await Testing.RunAsUserAsync("qqq@gmail.com", "1234");
+
+        var boardId = await Testing.SendAsync(new CreateBoardCommand("New Board"));
+
+        await Testing.SendAsync(new DeleteBoardCommand(boardId));
+
+        var table = await Testing.FindAsync<Board>(boardId);
+
+        table.Should().BeNull();
+    }
+}
