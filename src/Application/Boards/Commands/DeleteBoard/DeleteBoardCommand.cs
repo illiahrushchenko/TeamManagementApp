03c4074bdffc76a4db21cbc9742db00d9b424311
illia hrushchenko <gr.illya13@gmail.com>
2023-08-19 20:03:52 +0300

    Implemented DeleteBoardCommand

--- src/Application/Boards/Commands/DeleteBoard/DeleteBoardCommand.cs
+++ src/Application/Boards/Commands/DeleteBoard/DeleteBoardCommand.cs
@@ -1,4 +1,8 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Boards.Commands.DeleteBoard;
 
@@ -6,8 +10,30 @@ public record DeleteBoardCommand(int Id) : IRequest;
 
 public class DeleteBoardCommandHandler : IRequestHandler<DeleteBoardCommand>
 {
-    public Task Handle(DeleteBoardCommand request, CancellationToken cancellationToken)
+    
+    private readonly IApplicationDbContext _context;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IBoardMembersService _boardMembersService;
+
+    public DeleteBoardCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService, IBoardMembersService boardMembersService)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _currentUserService = currentUserService;
+        _boardMembersService = boardMembersService;
+    }
+
+    public async Task Handle(DeleteBoardCommand request, CancellationToken cancellationToken)
+    {
+        var board = await _context.Boards
+                        .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken) ??
+                    throw new NotFoundException(nameof(Board), request.Id);
+
+        if (!await _boardMembersService.UserIsOwner(_currentUserService.UserId, request.Id))
+        {
+            throw new ForbiddenAccessException(nameof(Board), request.Id);
+        }
+
+        _context.Boards.Remove(board);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }
