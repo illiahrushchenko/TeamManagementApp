71f1385226cdf8fbee3b4987723408bae8c64d75
illia hrushchenko <gr.illya13@gmail.com>
2023-08-20 13:22:57 +0300

    Implemented GetTableCardQuery

--- src/Application/TableCards/Queries/GetTableCard/GetTableCardQuery.cs
+++ src/Application/TableCards/Queries/GetTableCard/GetTableCardQuery.cs
@@ -1,14 +1,34 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
 using Application.Common.Models;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.TableCards.Queries.GetTableCard;
 
-public record GetTableCardQuery() : IRequest<TableCardDetailsDto>;
+public record GetTableCardQuery(int Id) : IRequest<TableCardDetailsDto>;
 
 public class GetTableCardQueryHandler : IRequestHandler<GetTableCardQuery, TableCardDetailsDto>
 {
-    public Task<TableCardDetailsDto> Handle(GetTableCardQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IBoardMembersService _boardMembersService;
+
+    public GetTableCardQueryHandler(IApplicationDbContext context, ICurrentUserService currentUserService, IBoardMembersService boardMembersService)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _currentUserService = currentUserService;
+        _boardMembersService = boardMembersService;
+    }
+
+    public async Task<TableCardDetailsDto> Handle(GetTableCardQuery request, CancellationToken cancellationToken)
+    {
+        var tableCard = await _context.TableCards
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken) ??
+                        throw new NotFoundException(nameof(TableCard), request.Id);
+
+        return new TableCardDetailsDto(tableCard.Id, tableCard.Title, tableCard.Description);
     }
 }
