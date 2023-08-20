69949a4e6fea731cb13b903b9673fff8fc7dc6d6
illia hrushchenko <gr.illya13@gmail.com>
2023-08-20 12:07:15 +0300

    Implemented DeleteTableCardCommand

--- src/Application/TableCards/Commands/DeleteTableCard/DeleteTableCardCommand.cs
+++ src/Application/TableCards/Commands/DeleteTableCard/DeleteTableCardCommand.cs
@@ -1,13 +1,40 @@
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.TableCards.Commands.DeleteTableCard;
 
-public record DeleteTableCardCommand() : IRequest;
+public record DeleteTableCardCommand(int Id) : IRequest;
 
 public class DeleteTableCardCommandHandler : IRequestHandler<DeleteTableCardCommand>
 {
-    public Task Handle(DeleteTableCardCommand request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IBoardMembersService _boardMembersService;
+
+    public DeleteTableCardCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService, IBoardMembersService boardMembersService)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _currentUserService = currentUserService;
+        _boardMembersService = boardMembersService;
+    }
+
+    public async Task Handle(DeleteTableCardCommand request, CancellationToken cancellationToken)
+    {
+        var tableCard = await _context.TableCards
+                            .Include(x => x.Table)
+                            .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken) ??
+                        throw new NotFoundException(nameof(TableCard), request.Id);
+
+        if (!await _boardMembersService.UserIsOwner(_currentUserService.UserId, tableCard.Table.BoardId) && 
+            !await _boardMembersService.UserIsMember(_currentUserService.UserId, tableCard.Table.BoardId))
+        {
+            throw new ForbiddenAccessException(nameof(Table), request.Id);
+        }
+
+        _context.TableCards.Remove(tableCard);
+        await _context.SaveChangesAsync(cancellationToken);
     }
 }
