d1a495cd3670d558cb398e026845eeba7874877c
illia hrushchenko <gr.illya13@gmail.com>
2023-08-22 14:21:21 +0300

    Implemented GetBoardDetailsQuery

--- src/Application/Boards/Queries/GetBoardDetails/GetBoardDetailsQuery.cs
+++ src/Application/Boards/Queries/GetBoardDetails/GetBoardDetailsQuery.cs
@@ -1,5 +1,10 @@
 using Application.Boards.Queries.GetBoardDetails.Dto;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Boards.Queries.GetBoardDetails;
 
@@ -7,8 +12,35 @@ public record GetBoardDetailsQuery(int Id) : IRequest<BoardDetailsDto>;
 
 public record GetBoardDetailsQueryHandler : IRequestHandler<GetBoardDetailsQuery, BoardDetailsDto>
 {
-    public Task<BoardDetailsDto> Handle(GetBoardDetailsQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _context;
+
+    public GetBoardDetailsQueryHandler(IApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+    }
+
+
+    public async Task<BoardDetailsDto> Handle(GetBoardDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var board = await _context.Boards
+                        .AsNoTracking()
+                        .Include(x => x.Tables)
+                            .ThenInclude(x => x.Cards)
+                        .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken) ??
+                    throw new NotFoundException(nameof(Board), request.Id);
+
+        var tableDtoList = new List<TableDetailsDto>();
+        
+        foreach (var table in board.Tables)
+        {
+            var tableCardDtoList = new List<TableCardDetailsDto>();
+            foreach (var card in table.Cards)
+            {
+                tableCardDtoList.Add(new TableCardDetailsDto(card.Id, card.Title, card.Description));
+            }
+            tableDtoList.Add(new TableDetailsDto(table.Id, table.Title, tableCardDtoList));
+        }
+
+        return new BoardDetailsDto(board.Id, board.Title, tableDtoList);
     }
 }
