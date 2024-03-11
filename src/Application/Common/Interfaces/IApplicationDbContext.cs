bebfc0e725ea03ba203dfff108e5a4e2a48cbe49
illia hrushchenko <gr.illya13@gmail.com>
2024-03-11 21:52:35 +0200

    Add new entities to IApplicationDbContext

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<Member> Members { get; }
+    DbSet<Invitation> Invitations { get; }
     DbSet<Board> Boards { get; }
     DbSet<Table> Tables { get; }
     DbSet<TableCard> TableCards { get; }
